3008fd75108bc20d761ee47e718390e08cf08d0f
James Vu <james.vu@zip.co>
2024-12-10 12:40:18 +1100

    create keymap for yanking filepath and use new keymap api

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -1,5 +1,7 @@
-local map = vim.api.nvim_set_keymap
-
-map('n', '<leader>st', ':TermSelect<CR>', { silent = true, noremap = true })
-map('n', '<leader>s,', ':ToggleTermSetName<CR>', { silent = true, noremap = true })
-map('n', '<tab>', '<C-6>', {})
+vim.keymap.set('n', '<leader>st', ':TermSelect<CR>', { silent = true, noremap = true })
+vim.keymap.set('n', '<leader>s,', ':ToggleTermSetName<CR>', { silent = true, noremap = true })
+vim.keymap.set('n', '<tab>', '<C-6>', {})
+vim.keymap.set('n', '<leader>yp', function()
+  vim.cmd 'let @+ = expand("%")'
+  vim.notify('Yanked filepath', vim.log.levels.INFO)
+end, { noremap = true, desc = '[Y]ank file[p]ath' })
