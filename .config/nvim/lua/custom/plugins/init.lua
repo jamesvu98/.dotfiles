30dfdb44146df8e403f6073984bc2f5a2330e31f
James Vu <james.vu@zip.co>
2024-12-11 12:30:50 +1100

    add nvim-scrollbar plugin

--- .config/nvim/lua/custom/plugins/init.lua
+++ .config/nvim/lua/custom/plugins/init.lua
@@ -1,3 +1,4 @@
+local colors = require('tokyonight.colors').setup()
 -- You can add your own plugins here or in other files in this directory!
 --  I promise not to create any merge conflicts in this directory :)
 --
@@ -9,6 +10,25 @@ return {
     opts = {
       open_mapping = '<M-i>',
       direction = 'float',
+    },
+  },
+  {
+    'petertriho/nvim-scrollbar',
+    opts = {
+      handle = {
+        color = colors.bg_highlight,
+      },
+      marks = {
+        Search = { color = colors.orange },
+        Error = { color = colors.error },
+        Warn = { color = colors.warning },
+        Info = { color = colors.info },
+        Hint = { color = colors.hint },
+        Misc = { color = colors.purple },
+      },
+      handlers = {
+        gitsigns = true,
+      },
     },
   },
 }
